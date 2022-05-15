0fab6f7067b1383600b8239e80080cfc54d664fa
MohammadHuseyn <mohammadhuseynamini@gmail.com>
2022-05-15 23:27:25 +0430

    some comment added

--- lib/Classes/User.dart
+++ lib/Classes/User.dart
@@ -1,20 +1,20 @@
 import 'package:flutter/material.dart';
 class User {
   final int id = DateTime.now().millisecondsSinceEpoch;
-  String username;
+  String username; // need to be unique
   String firstName;
   String lastName;
-  String password;
-  List<User> following = [];
-  List<User> followers = [];
-  List</*TODO: make post file*/> posts = [];
-  final DateTime dateJoined = DateTime.now();
+  String password; // need a double check
+  List<User> following = []; // is empty at first
+  List<User> followers = []; // is empty at first
+  List</*TODO: make post file*/> posts = []; // is empty at first
+  final DateTime dateJoined = DateTime.now(); // no one can change that
 
   User({
-    this.username,
-    this.firstName,
-    this.lastName,
-    this.password,
+    this.username,  //
+    this.firstName, // nothing
+    this.lastName,  // can be
+    this.password,  // empty
 });
 
 }
