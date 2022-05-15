68c3cc31c10d833dad98c6d4a141bd416538aa45
MohammadHuseyn <mohammadhuseynamini@gmail.com>
2022-05-15 15:25:09 +0430

    making widgets of switcherView(bar)

--- lib/bottomSwitcher.dart
+++ lib/bottomSwitcher.dart
@@ -1,18 +1,24 @@
 import 'package:flutter/material.dart';
-class BottomSwithcer extends StatefulWidget {
-  const BottomSwithcer({Key key}) : super(key: key);
+import 'package:test_mu/bottomSwitcher/comment.dart';
+import 'package:test_mu/bottomSwitcher/notifications.dart';
+import 'package:test_mu/bottomSwitcher/home.dart';
+import 'package:test_mu/bottomSwitcher/add.dart';
+import 'package:test_mu/bottomSwitcher/search.dart';
+class BottomSwitcher extends StatefulWidget {
 
   @override
-  State<BottomSwithcer> createState() => _BottomSwithcerState();
+  State<BottomSwitcher> createState() => _BottomSwitcherState();
 }
 
-class _BottomSwithcerState extends State<BottomSwithcer> {
+class _BottomSwitcherState extends State<BottomSwitcher> {
   int index = 0;
-
   static List<Widget> _swithcerView = [
-
+    Home(),
+    Search(),
+    Add(),
+    Comment(),
+    Notifications(),
   ];
-
   @override
   Widget build(BuildContext context) {
     return Container();
---
+++ lib/bottomSwitcher/comment.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+class Comment extends StatelessWidget {
+  const Comment({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Text("Comment"),
+      ),
+    );
+  }
+}
