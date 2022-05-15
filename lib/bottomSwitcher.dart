7458a453d6b501ce3c986638fdc6fdfeddfdb43b
MohammadHuseyn <mohammadhuseynamini@gmail.com>
2022-05-15 15:13:56 +0430

    Creating bottomSwitcher.dart to use the switcher bar

---
+++ lib/bottomSwitcher.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+class BottomSwithcer extends StatefulWidget {
+  const BottomSwithcer({Key key}) : super(key: key);
+
+  @override
+  State<BottomSwithcer> createState() => _BottomSwithcerState();
+}
+
+class _BottomSwithcerState extends State<BottomSwithcer> {
+  int index = 0;
+
+  static List<Widget> _swithcerView = [
+
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
