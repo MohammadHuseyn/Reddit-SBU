eca4e4721f97396f42a2f8485d909c8925d97ae6
hgor1382 <hgor1382@gmail.com>
2022-05-19 11:25:40 +0430

    Creating base of Sign up page

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:test_mu/Authentication/signup.dart';
 import 'bottomSwitcher.dart';
 void main() {
   runApp(MyApp());
@@ -24,7 +25,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: BottomSwitcher(),
+      home: Signuppage(),
     );
   }
 }
