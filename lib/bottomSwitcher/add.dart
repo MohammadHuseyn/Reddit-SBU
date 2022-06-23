d08670816e7da2b9888b540b1e6cdd787ceaac16
MohammadHuseyn <mohammadhuseynamini@gmail.com>
2022-06-23 20:50:04 +0430

    add new post and add new comminuty page fixed

--- lib/bottomSwitcher/add.dart
+++ lib/bottomSwitcher/add.dart
@@ -1,10 +1,8 @@
-import 'package:file_picker/file_picker.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:test_mu/Classes/Community.dart';
-import 'package:test_mu/Classes/Post.dart';
-import 'package:test_mu/PageNavigator.dart';
-
+import 'addComminuty.dart';
 import '../Classes/User.dart';
+import 'addPost.dart';
 
 class Add extends StatefulWidget {
   Add({this.mainUser});
@@ -16,85 +14,57 @@ class Add extends StatefulWidget {
 }
 
 class _AddState extends State<Add> {
-  final captionC = TextEditingController();
-  final imageDirectoryC = TextEditingController();
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.black87,
-      alignment: Alignment.center,
-      padding: EdgeInsets.only(left: 100, right: 100, top: 50),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        // mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Container(
-            width: 400,
-            child: DropdownButton<Community>(
-              hint: Text("  Select a community to publish post  ",style: TextStyle(color: Colors.white),),
-              items: widget.mainUser.communities.map((Community value) {
-                return DropdownMenuItem<Community>(
-                  value: value,
-                  child: Text(value.name),
-                );
-              }).toList(),
-              onChanged: (_) {},
+    return Scaffold(
+        backgroundColor: Colors.black,
+        appBar: AppBar(
+          backgroundColor: Colors.black54,
+          title: Text("Select Comminuty or post"),
+        ),
+        body: Column(
+          children: [
+            SizedBox(
+              height: 50,
             ),
-          ),
-          Theme(
-            data: new ThemeData(
-              primaryColor: Colors.white,
-              primaryColorDark: Colors.white,
+            Center(
+                child: ElevatedButton(
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => AddComminuty(
+                                    mainUser: widget.mainUser,
+                                  )));
+                    },
+                    style:
+                        ElevatedButton.styleFrom(padding: EdgeInsets.all(30),
+                        primary: Colors.white),
+                    child: Text(
+                      "Comminuty",
+                      style: TextStyle(fontSize: 20,color: Colors.black),
+                    ))),
+            SizedBox(
+              height: 50,
             ),
-            child: TextField(
-              controller: captionC,
-              style: TextStyle(color: Colors.white),
-              decoration: InputDecoration(
-                border: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.white)),
-                hintText: 'Tyepe your caption for new post',
-                hintStyle: TextStyle(color: Colors.white),
-                labelStyle: TextStyle(color: Colors.white),
-                enabledBorder: OutlineInputBorder(
-                    borderSide: const BorderSide(color: Colors.white)),
-                labelText: 'Caption',
-                prefixIcon: Icon(
-                  Icons.short_text_rounded,
-                  color: Colors.white,
-                ),
-              ),
+            Center(
+              child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(padding: EdgeInsets.all(30),
+                  primary: Colors.white),
+                  onPressed: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => AddPost(
+                                  mainUser: widget.mainUser,
+                                )));
+                  },
+                  child: Text(
+                    "Post",
+                    style: TextStyle(fontSize: 20,color: Colors.black),
+                  )),
             ),
-          ),
-          SizedBox(
-            height: 50,
-          ),
-          // ElevatedButton(
-          //     onPressed: () async {
-          //       FilePickerResult result = await FilePicker.platform.pickFiles();
-          //       if (result != null) {
-          //         print(result.files.first.name);
-          //       }
-          //       imageDirectoryC.text = "assets/google.png";
-          //     },
-          //     child: Text(
-          //       "choose image for new post",
-          //       style: TextStyle(color: Colors.white),
-          //     )),
-          SizedBox(
-            height: 50,
-          ),
-          ElevatedButton(
-              onPressed: () {
-                widget.mainUser.communities.elementAt(0).posts.add(new Post(
-                    caption: captionC.text,
-                    // imageDirectory: "assets/google.png",
-                    owner: widget.mainUser));
-                captionC.clear();
-              },
-              style: ElevatedButton.styleFrom(padding: EdgeInsets.all(30)),
-              child: Text("Upload new post",style: TextStyle(fontSize: 20),))
-        ],
-      ),
-    );
+          ],
+        ));
   }
 }
---
+++ lib/bottomSwitcher/addPost.dart
@@ -0,0 +1,109 @@
+import 'package:file_picker/file_picker.dart';
+import 'package:flutter/material.dart';
+import 'package:test_mu/Classes/Community.dart';
+import 'package:test_mu/Classes/Post.dart';
+import 'package:test_mu/PageNavigator.dart';
+
+import '../Classes/User.dart';
+
+class AddPost extends StatefulWidget {
+  AddPost({this.mainUser});
+
+  User mainUser;
+
+  @override
+  State<AddPost> createState() => _AddPostState();
+}
+
+class _AddPostState extends State<AddPost> {
+  final captionC = TextEditingController();
+  final imageDirectoryC = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Add a new post"),
+        backgroundColor: Colors.black54,
+      ),
+      body: Container(
+        color: Colors.black87,
+        alignment: Alignment.center,
+        padding: EdgeInsets.only(left: 100, right: 100, top: 50),
+        child: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            // mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                width: 400,
+                child: DropdownButton<Community>(
+                  hint: Text("  Select a community to publish post  ",style: TextStyle(color: Colors.white),),
+                  items: widget.mainUser.communities.map((Community value) {
+                    return DropdownMenuItem<Community>(
+                      value: value,
+                      child: Text(value.name),
+                    );
+                  }).toList(),
+                  onChanged: (_) {},
+                ),
+              ),
+              SizedBox(height:20,),
+              Theme(
+                data: new ThemeData(
+                  primaryColor: Colors.white,
+                  primaryColorDark: Colors.white,
+                ),
+                child: TextField(
+                  controller: captionC,
+                  style: TextStyle(color: Colors.white),
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.white)),
+                    hintText: 'Tyepe your caption for new post',
+                    hintStyle: TextStyle(color: Colors.white),
+                    labelStyle: TextStyle(color: Colors.white),
+                    enabledBorder: OutlineInputBorder(
+                        borderSide: const BorderSide(color: Colors.white)),
+                    labelText: 'Caption',
+                    prefixIcon: Icon(
+                      Icons.short_text_rounded,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
+              // SizedBox(
+              //   height: 50,
+              // ),
+              // ElevatedButton(
+              //     onPressed: () async {
+              //       FilePickerResult result = await FilePicker.platform.pickFiles();
+              //       if (result != null) {
+              //         print(result.files.first.name);
+              //       }
+              //       imageDirectoryC.text = "assets/google.png";
+              //     },
+              //     child: Text(
+              //       "choose image for new post",
+              //       style: TextStyle(color: Colors.white),
+              //     )),
+              SizedBox(
+                height: 50,
+              ),
+              ElevatedButton(
+                  onPressed: () {
+                    widget.mainUser.communities.elementAt(0).posts.add(new Post(
+                        caption: captionC.text,
+                        // imageDirectory: "assets/google.png",
+                        owner: widget.mainUser));
+                    captionC.clear();
+                  },
+                  style: ElevatedButton.styleFrom(padding: EdgeInsets.all(30)),
+                  child: Text("Upload new post",style: TextStyle(fontSize: 20),))
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
