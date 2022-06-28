87f58ee39ad98fab52495dc81cd4cef18cc265df
MohammadHuseyn <mohammadhuseynamini@gmail.com>
2022-06-28 13:39:30 +0430

    changing theme

--- lib/bottomSwitcher/SerachResult.dart
+++ lib/bottomSwitcher/SerachResult.dart
@@ -8,128 +8,101 @@ import '../Classes/User.dart';
 import 'PostItem.dart';
 
 class SearchResult extends StatefulWidget {
-  SearchResult({this.mainUser,this.communities, this.query});
+  SearchResult({this.mainUser, this.communities, this.query});
 
   List<Community> communities;
   String query;
   User mainUser;
+
   @override
   State<SearchResult> createState() => _SearchResultState();
 }
 
 class _SearchResultState extends State<SearchResult> {
-  String str ="Unkown";
+  String str = "Unkown";
+
   @override
   Widget build(BuildContext context) {
-
     // TODO: implement buildResults
     Community community;
     for (int i = 0; i < widget.communities.length; i++) {
-      if (widget.communities
-          .elementAt(i)
-          .name == widget.query) {
+      if (widget.communities.elementAt(i).name == widget.query) {
         community = widget.communities.elementAt(i);
         break;
       }
     }
-    str = mainUser.communities.contains(community)?"UnFollow":"Follow";
-    changeFollow(){
+    str = mainUser.communities.contains(community) ? "UnFollow" : "Follow";
+    changeFollow() {
       setState(() {
         bool flag = mainUser.communities.contains(community);
-        str = flag?"UnFollow":"Follow";
+        str = flag ? "UnFollow" : "Follow";
         if (flag)
-          mainUser.communities.removeAt(mainUser.communities.indexOf(community));
-        else mainUser.communities.add(community);
+          mainUser.communities
+              .removeAt(mainUser.communities.indexOf(community));
+        else
+          mainUser.communities.add(community);
       });
     }
-    return Container(
-      alignment: Alignment.topLeft,
-      padding: EdgeInsets.all(20),
-      child: SingleChildScrollView(
-        child: Stack(
-            children: <Widget>[
-        Container(
-        color: Colors.black,
-            child: Column(
-                children: [
-                Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  // ClipOval(
-                  //   child: Image.asset(
-                  //     community.avatarDirectory,
-                  //     width: 175,
-                  //   ),
-                  // ),
-                  Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                  RichText(
-                  text: TextSpan(
-                  children: [
-                      TextSpan(
-                      text: "Tilte: ",
-                      style: TextStyle(color: Colors.white54)),
-                  TextSpan(text: community.name + "\n"),
-                  TextSpan(
-                      text: "Description: ",
-                      style: TextStyle(color: Colors.white54)),
-                  TextSpan(text: community.descriptoin + "\n"),
-                  TextSpan(
-                      text: "Description: ",
-                      style: TextStyle(color: Colors.white54)),
-                  TextSpan(text: community.descriptoin + "\n"),
-                  TextSpan(
-                      text: "owner: ",
-                      style: TextStyle(color: Colors.white54)),
-                  TextSpan(text: community.owner.username + "\n"),
-                  TextSpan(
-                      text: "number of followers: ",
-                      style: TextStyle(color: Colors.white54)),
-                  TextSpan(
-                      text:
-                      community.followers.length.toString() +
-                          "\n"),
-                  TextSpan(
-                      text: "number of posts: ",
-                      style: TextStyle(color: Colors.white54)),
-                  TextSpan(
-                      text: community.posts.length.toString() +
-                          "\n"),
-                ],
-                style: TextStyle(
-                    fontSize: 15,
-                    height: 1.8,
-                    color: Colors.white))),
-        ElevatedButton(
-            onPressed: () {
-              changeFollow();
-            }, child: Text(str)),
+
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text(
+          community.owner.username,
+        ),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: SingleChildScrollView(
+              child: Container(
+                height: MediaQuery.of(context).size.height - 50,
+                child: ListView.builder(
+                  itemCount: community.posts.length,
+                  itemBuilder: (context, index) {
+                    return PostItem(
+                      post: community.posts.elementAt(index),
+                    );
+                  },
+                ),
+              ),
+            ),
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  fixedSize: Size(MediaQuery.of(context).size.width, 50),
+                ),
+                onPressed: () {
+                  showModalBottomSheet<void>(
+                    context: context,
+                    builder: (BuildContext context) {
+                      return Container(
+                        height: 200,
+                        color: Colors.amber,
+                        child: Center(
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            mainAxisSize: MainAxisSize.min,
+                            children: <Widget>[
+                              const Text('Modal BottomSheet'),
+                              ElevatedButton(
+                                child: const Text('Close BottomSheet'),
+                                onPressed: () => Navigator.pop(context),
+                              )
+                            ],
+                          ),
+                        ),
+                      );
+                    },
+                  );
+                },
+                child: Text("Comminuty detail")),
+          )
         ],
-      )
-      ],
-    ),
-    Divider(
-    color: Colors.white,
-    ),
-    Container(
-    height: 500,
-    child: ListView.builder(
-    itemCount: community.posts.length,
-    itemBuilder: (context, index) {
-    return PostItem(
-    post: community.posts.elementAt(index));
-    }),
-    ),
-    ],
-    ),
-    ),
-    ]
-    ,
-    )
-    )
+      ),
     );
   }
 }
