7b2d145cadfb2d01cb7b1288d58e9d02d9459c89
MohammadHuseyn <mohammadhuseynamini@gmail.com>
2022-06-28 15:15:49 +0430

    bottom popup finished

--- lib/bottomSwitcher/SerachResult.dart
+++ lib/bottomSwitcher/SerachResult.dart
@@ -4,8 +4,6 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:test_mu/Classes/Community.dart';
 import 'package:test_mu/PageNavigator.dart';
-import 'package:test_mu/main.dart';
-
 import '../Classes/User.dart';
 import 'PostItem.dart';
 
@@ -33,16 +31,21 @@ class _SearchResultState extends State<SearchResult> {
         break;
       }
     }
-    str = mainUser.communities.contains(community) ? "UnFollow" : "Follow";
+    str = mainUser.communities.contains(community)
+        ? "\nUnfollow \n"
+        : "\nFollow \n";
     changeFollow() {
       setState(() {
         bool flag = mainUser.communities.contains(community);
-        str = flag ? "UnFollow" : "Follow";
-        if (flag)
+        str = flag ? "\nUnfollow \n" : "\nFollow \n";
+        if (flag) {
           mainUser.communities
               .removeAt(mainUser.communities.indexOf(community));
-        else
+          community.followers.remove(mainUser);
+        } else {
           mainUser.communities.add(community);
+          community.followers.add(mainUser);
+        }
       });
     }
 
@@ -80,52 +83,143 @@ class _SearchResultState extends State<SearchResult> {
                 onPressed: () {
                   showModalBottomSheet<void>(
                     context: context,
-                    builder: (BuildContext context) {
-                      return Container(
-                        height: 500,
-                        color: Colors.indigo,
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: [
-                            Transform.rotate(
-                              angle: 3 * pi / 2,
-                              child: Padding(
-                                  padding: EdgeInsets.all(10),
-                                  child: Icon(Icons.arrow_forward_ios_rounded,color: Colors.white,)),
-                            ),
-                            SizedBox(height: 20,),
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                Column(
-                                  children: [
-                                    Text("Posts", style: TextStyle(color: Colors.white,fontSize: 25),),
-                                    SizedBox(height: 20,),
-                                    Text(community.posts.length.toString(), style: TextStyle(color: Colors.white,fontSize: 25),)
-                                  ],
-                                ),
-                                SizedBox(width: 60,),
-                                Column(
-                                  children: [
-                                    Text("Followers",style: TextStyle(color: Colors.white,fontSize: 25),),
-                                    SizedBox(height: 20,),
-                                    Text(community.followers.length.toString(), style: TextStyle(color: Colors.white,fontSize: 25),)
-                                  ],
-                                ),
-                                SizedBox(width: 60,),
-                                Column(
-                                  children: [
-                                    Text("Admins",style: TextStyle(color: Colors.white,fontSize: 25),),
-                                    SizedBox(height: 20,),
-                                    Text(community.admins.length.toString(), style: TextStyle(color: Colors.white,fontSize: 25),)
-                                  ],
-                                ),
-                              ],
-                            )
-                          ],
-                        ),
-                      );
+                    builder: (context) {
+                      return StatefulBuilder(builder: (BuildContext context,
+                          StateSetter setState) {
+                        return Container(
+                          height: 500,
+                          color: Colors.indigo,
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              Transform.rotate(
+                                angle: 3 * pi / 2,
+                                child: Padding(
+                                    padding: EdgeInsets.all(10),
+                                    child: IconButton(
+                                      icon: Icon(Icons.arrow_forward_ios_rounded),
+                                      color: Colors.white,
+                                      onPressed: () => Navigator.pop(context),
+                                    )),
+                              ),
+                              SizedBox(
+                                height: 20,
+                              ),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Column(
+                                    children: [
+                                      Text(
+                                        "Posts",
+                                        style: TextStyle(
+                                            color: Colors.white, fontSize: 25),
+                                      ),
+                                      SizedBox(
+                                        height: 20,
+                                      ),
+                                      Text(
+                                        community.posts.length.toString(),
+                                        style: TextStyle(
+                                            color: Colors.white, fontSize: 25),
+                                      )
+                                    ],
+                                  ),
+                                  SizedBox(
+                                    width: 60,
+                                  ),
+                                  Column(
+                                    children: [
+                                      Text(
+                                        "Followers",
+                                        style: TextStyle(
+                                            color: Colors.white, fontSize: 25),
+                                      ),
+                                      SizedBox(
+                                        height: 20,
+                                      ),
+                                      Text(
+                                        community.followers.length.toString(),
+                                        style: TextStyle(
+                                            color: Colors.white, fontSize: 25),
+                                      )
+                                    ],
+                                  ),
+                                  SizedBox(
+                                    width: 60,
+                                  ),
+                                  Column(
+                                    children: [
+                                      Text(
+                                        "Admins",
+                                        style: TextStyle(
+                                            color: Colors.white, fontSize: 25),
+                                      ),
+                                      SizedBox(
+                                        height: 20,
+                                      ),
+                                      Text(
+                                        community.admins.length.toString(),
+                                        style: TextStyle(
+                                            color: Colors.white, fontSize: 25),
+                                      )
+                                    ],
+                                  ),
+                                ],
+                              ),
+                              SizedBox(
+                                height: 25,
+                              ),
+                              Text(
+                                community.name,
+                                style: TextStyle(
+                                    color: Colors.white, fontSize: 20),
+                              ),
+                              SizedBox(
+                                height: 10,
+                              ),
+                              Text(
+                                community.descriptoin,
+                                style: TextStyle(
+                                    color: Colors.white, fontSize: 20),
+                              ),
+                              SizedBox(
+                                height: 10,
+                              ),
+                              Text(
+                                "belongs to " + community.owner.username,
+                                style: TextStyle(
+                                    color: Colors.white, fontSize: 20),
+                              ),
+                              SizedBox(
+                                height: 10,
+                              ),
+                              ElevatedButton(
+                                onPressed: () {
+                                  changeFollow();
+                                  Navigator.pop(context);
+                                },
+                                child: Row(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: [
+                                      Text(
+                                        str,
+                                        style: TextStyle(fontSize: 20),
+                                      ),
+                                      widget.mainUser.communities
+                                              .contains(community)
+                                          ? Icon(
+                                              Icons.clear,
+                                              size: 20,
+                                            )
+                                          : Icon(Icons.check)
+                                    ]),
+                              )
+                            ],
+                          ),
+                        );
+                      });
                     },
                   );
                 },
